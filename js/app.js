41bd34208ab1046a44c2cee2eff9790b73b4a6a1
Codepressd <creederone@gmail.com>
2016-07-19 10:00:21 -0700

    Created an array of objects that houses all the questions

---
+++ js/app.js
@@ -0,0 +1,38 @@
+/*Log all five questions, answers and correct answers*/
+var allQuestions = [
+	{
+		id: 1,
+		question: "Who was the Godfather of modern mixology",
+		answers: ["Dale Degroff", "Jerry Thomas", "Patrick Swayze"],
+		correct: 2
+	},
+	{
+		id: 2,
+		question: "What is the Official drink of The Preakness?",
+		answers: ["Mint Julep", "The Manhattan", "Black-Eyed Susan"],
+		correct: 3
+	},
+	{
+		id: 3,
+		question: "What is the oldest recorded cocktail?",
+		answers: ["El Draque", "Old Fasihoned", "Sazarac"],
+		correct: 1
+	},
+	{
+		id: 4,
+		question: "What was the original liquor used in a Sazarac?",
+		answers: ["Rye Whiskey", "Bourbon", "Cognac"],
+		correct: 2
+	},
+	{
+		id: 5,
+		question: "Which of these were a popular speakeasy during prohibition?",
+		answers: ["Cotton Club", "21 Club", "Stork Club"],
+		correct: 0
+	}
+
+];
+
+/*Function that edits the DOM */
+
+/*Function that compares if a question is right*/
